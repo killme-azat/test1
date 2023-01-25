5c56269dd9202066fa21cc7369ae649c34eb72e6
kudes <kasenov2002@mail.ru>
2023-01-25 21:07:47 +0600

    solving problems 1-5

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-let age = 18;
-console.log(age);
-if (age >= 18) {
-    console.log("Ты взрослый")
-} else {
-    console.log("Ты маленький")
+// function greeting() {
+//     console.log("Hello World!");
+// }
+
+// greeting();
+
+
+// function loop() {
+//     for (let i = 0; i < 10; i++)
+//         console.log(i);
+// }
+
+// loop();
+
+
+// function sum(a, b) {
+//     // let c = a + b;
+//     console.log(a + b);
+// }
+
+// sum(1, 2);
+// sum(-5, 7);
+
+
+// function less(a) {
+//     let b = a - 1;
+//     return b;
+// }
+
+// function more(a) {
+//     let b = a + 1;
+//     return b;
+// }
+
+// const res1 = more(1);
+// console.log(res1);
+
+// const res2 = less(-5);
+// console.log(res2);
+
+function getLength(string) {
+    return string.length;
 }
 
-console.log(object);
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);    
-}
+console.log(getLength("Privet"));
